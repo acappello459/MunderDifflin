944b8f02701025da190488cd39d137e987e00b22
acappello459 <acappello459@outlook.com>
2018-10-01 00:26:22 -0400

    added most of the data

--- index.js
+++ index.js
@@ -20,31 +20,113 @@
 // Does my website fulfil the needs of the user?
 // Do my animations and event callbacks improve the user experience?
 // Does my website describe its intent?
-var wrapperA = document.getElementById('wrapper a');
-var wrapperB = document.getElementById('wrapper b');
-var wrapperC = document.getElementById('wrapper c');
-var logo = document.createElement('div')
-var whatever = document.getElementById('whatever')
+var logo = document.getElementById('logo')
+var wrapApA = document.getElementById('pA')
+var modal = document.createElement('div')
+var container = document.getElementsByClassName('container')
+var xButton = document.createElement('button')
+var enter = document.createElement('button')
+var modalText = document.createElement('div')
+var modalInput = document.createElement('input')
+// var wrapAimg = document.createElement('img')
 
-// logo.style.display = 'fixed';
-logo.style.border = '2px solid'
-logo.style.height = '150px'
-logo.style.width = '250px'
-logo.style.borderRadius = '30%';
-// logo.style.zIndex= '2';
-logo.style.backgroundImage = "url('images/logo.jpg')"
-logo.style.backgroundSize ='100% 100%'
-whatever.appendChild(logo)
+logo.style.float = 'left'
+logo.style.borderRadius = '0% 25% 0% 25%'
+logo.style.position = 'relatve'
+
+// logo.style.margin = '-125px 0px'
+// document.body.appendChild(logo)
+
+wrapApA.style.width = '35%'
+wrapApA.style.margin = '20px'
+function popup(){
+
+  // The styling of the Modal
+  modal.style.height = '750px';
+  modal.style.width = '75%';
+  modal.style.marginTop = '-100%'
+  modal.style.marginLeft = '15%'
+  modal.style.backgroundColor = '#42a4f4';
+  modal.style.backgroundImage = 'none'
+  modal.style.zIndex = '3'
+  modal.style.position = 'absolute';
+  // modal.style.opacity = '.9'
+  document.body.appendChild(modal)
+
+// The styling of the x out button
+  xButton.style.height = '30px';
+  xButton.style.width = '30px';
+  xButton.style.border = 'none'
+  // xButton.style.backgroundColor = 'lightgrey';
+  xButton.style.background='none';
+  xButton.innerHTML = 'X';
+  xButton.style.fontSize = '2em';
+  xButton.style.marginLeft = '97.25%';
+  modal.appendChild(xButton)
+
+//Text for explaining the input box
+  modalText.style.border = '2px solid';
+  modalText.style.width = '50%';
+  modalText.style.margin= '5% auto';
+  modalText.style.textAlign = 'center';
+  modalText.innerHTML ="Please enter your email address below. We will add you to a call list so that you can recieve updates on our stock, prices, and recieve coupon deals. Save money by doing business with Munder Difflin!";
+  modalText.style.fontSize = '3em'
+  modal.appendChild(modalText)
+
+//input box asking for the user's email address
+  modalInput.style.width ='30%';
+  modalInput.style.height = '3%';
+  modalInput.style.marginBottom = '500px';
+  modalInput.style.marginLeft = '35%'
+  modal.appendChild(modalInput)
+
+//enter button styling
+  enter.style.height = '25px';
+  // enter.style.width = '25px';
+  enter.style.backgroundColor = 'lightgrey';
+  enter.innerHTML = 'ENTER';
+  enter.style.marginLeft = '2%'
+  // enter.style.marginLeft = '97.25%'
+  modal.appendChild(enter)
+}
+
+// function modalSize(){
+//   if (x.matches){
+//     modal.style.marginTop = '-250%'
+//   }
+// }
+// var x = window.matchMedia(maxWidth = '1100px')
+// modalSize(x)
+// x.addListener(modalSize)
 
 
-// wrapper1.style.zIndex = '1';
-// wrapperA.style.margin = '0px 0px'
-// wrapperA.style.position = 'absolute'
-wrapperA.style.backgroundColor = 'blue'
+//function that tells the modal to pop up 1 second after the page loads
+function start(){
+  setTimeout(popup,300)
+}
+window.addEventListener('load', start)
 
+//when you click the x button it gets rid of the modal
+xButton.addEventListener('click',function(){
+  modal.style.display = 'none'
+  xButton.style.display = 'none'
+})
 
-wrapperB.style.backgroundColor = 'red'
-// wrapper.style.height = '400px'
+//when you click the input box it gets rid of the text so you don't have to delete 'Thank you' if you wanted to enter in more than one email
+modalInput.addEventListener('click', function(){
+  modalInput.value = ''
+})
 
+var newArr = [];
+//eventlistener that when you click the enter button it checks to see if there is an @ symbol. if not it will say to reenter the email address
+enter.addEventListener('click', function(){
 
-wrapperC.style.backgroundColor = 'blue'
+  newArr.push(modalInput.value)
+  for(let i = 0; i<newArr.length; i++){
+  if(newArr[i][i] == '@'){
+    console.log('Thanks')
+  }else{
+    console.log('try again')
+  }
+}
+})
